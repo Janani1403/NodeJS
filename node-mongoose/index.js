838e961b306ca6f1cb55351635c580a62a5094aa
Janani1403 <janani.haran1403@gmail.com>
2020-04-16 14:23:16 +0530

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -31,5 +31,25 @@ connect.then((db) => {
         .catch((err) => {
             console.log(err);
         });
+	Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 });
