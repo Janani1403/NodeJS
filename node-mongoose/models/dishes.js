cf4e2e71da03a3080ca07bd176ad78daecedaea1
Janani1403 <janani.haran1403@gmail.com>
2020-04-16 16:09:03 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,3 +1,6 @@
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -20,7 +23,7 @@ var commentSchema = new Schema({
     timestamps: true
 });
 
-var dishSchema = new Schema({
+const dishSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -30,11 +33,31 @@ var dishSchema = new Schema({
         type: String,
         required: true
     },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
     comments:[commentSchema]
 }, {
     timestamps: true
 });
-
 var Dishes = mongoose.model('Dish', dishSchema);
 
 module.exports = Dishes;
